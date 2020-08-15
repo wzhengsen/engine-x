20df70c3ff5ce07c5ca51dea632027b1b7a939d2
XiaoLong <xiaolong@dcpt.online>
2020-08-15 21:46:18 +0800

    [chg]PXFileStream打开文件时，在windows上默认使用utf-8路径。

--- cocos/platform/CCFileStream.cpp
+++ cocos/platform/CCFileStream.cpp
@@ -18,6 +18,7 @@
 #define posix_lseek ::_lseek
 #define posix_read ::_read
 #define posix_write ::_write
+#include "iconv.h"
 #else
 #define O_READ_FLAGS O_RDONLY, S_IRUSR
 #define O_WRITE_FLAGS O_CREAT | O_RDWR, S_IRWXU
@@ -141,7 +142,26 @@ bool FileStream::open(const std::string& path, FileStream::Mode mode)
 {
     bool ok = false;
 #if CC_TARGET_PLATFORM != CC_PLATFORM_ANDROID
-    ok = pfs_posix_open(path, mode, _handle) != -1;
+    std::string _path = path;
+#ifdef _WIN32
+    auto i = iconv_open("gb2312//TRANSLIT", "utf-8");
+    if (i != (iconv_t)-1 && !path.empty()) {
+        size_t inLen = path.length();
+        size_t outLen = inLen;
+        size_t cvtLen = outLen;
+        char* inBuffer = const_cast<char*>(path.c_str());
+        char* outBuffer = new char[outLen];
+
+        const auto cvCount = iconv(i, const_cast<char**>(&inBuffer), &inLen, &outBuffer, &cvtLen);
+        iconv_close(i);
+        if (cvCount != static_cast<size_t>(-1)) {
+            _path = std::string(outBuffer, outLen - cvtLen);
+        }
+        delete[] outBuffer;
+    }
+
+#endif
+    ok = pfs_posix_open(_path, mode, _handle) != -1;
 #else // Android
     if (path[0] != '/') { // from package, always readonly
         std::string relativePath;
