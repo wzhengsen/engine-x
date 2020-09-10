5ffa7ec073d0fec3f0ba0e359ed075c125de17c4
wzhengsen <wzhengsen@gmail.com>
2020-09-10 13:54:11 +0800

    [chg]Initialize gtk before use.

--- cocos/ui/UIEditBox/UIEditBoxImpl-linux.h
+++ cocos/ui/UIEditBox/UIEditBoxImpl-linux.h
@@ -2,19 +2,19 @@
  Copyright (c) 2010-2012 cocos2d-x.org
  Copyright (c) 2015 hanxi
  Copyright (c) 2017-2018 Xiamen Yaji Software Co., Ltd.
- 
+
  http://www.cocos2d-x.org
- 
+
  Permission is hereby granted, free of charge, to any person obtaining a copy
  of this software and associated documentation files (the "Software"), to deal
  in the Software without restriction, including without limitation the rights
  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
  copies of the Software, and to permit persons to whom the Software is
  furnished to do so, subject to the following conditions:
- 
+
  The above copyright notice and this permission notice shall be included in
  all copies or substantial portions of the Software.
- 
+
  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -53,7 +53,7 @@ public:
      * @lua NA
      */
     virtual ~EditBoxImplLinux();
-    
+
 
     virtual bool isEditing() override;
     virtual void createNativeControl(const Rect& frame) override {};
@@ -69,12 +69,12 @@ public:
     virtual void setNativePlaceHolder(const char* pText) override {};
     virtual void setNativeVisible(bool visible) override {};
     virtual void updateNativeFrame(const Rect& rect) override {};
-    virtual const char* getNativeDefaultFontName() override {};
+    virtual const char* getNativeDefaultFontName() override {return "";};
     virtual void nativeOpenKeyboard() override;
     virtual void nativeCloseKeyboard() override {};
     virtual void setNativeMaxLength(int maxLength) override {};
 
-    
+
 private:
     virtual void doAnimationWhenKeyboardMove(float duration, float distance)override {}
 };
@@ -88,4 +88,3 @@ NS_CC_END
 #endif /* #if (CC_TARGET_PLATFORM == CC_PLATFORM_LINUX) */
 
 #endif /* __UIEDITBOXIMPLLINUX_H__ */
-
