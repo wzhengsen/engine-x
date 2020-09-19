146e4a9f191928cd76e66370ea07b167360376e7
wzhengsen <wzhengsen@gmail.com>
2020-09-20 00:42:49 +0800

    [new]Windows libcef.

--- cocos/platform/windows/CCApplication-windows.cpp
+++ cocos/platform/windows/CCApplication-windows.cpp
@@ -33,6 +33,7 @@ THE SOFTWARE.
 #include <VersionHelpers.h>
 #define NTCVT_CP_DEFAULT CP_UTF8
 #include "windows-specific/ntcvt/ntcvt.hpp"
+#include "include/cef_app.h"
 
 /**
 @brief    This function change the PVRFrame show/hide setting in register.
@@ -128,22 +129,20 @@ void Application::NotifyProc(HWND hwnd, WPARAM wParam, LPARAM lParam) {
         }
         MapNotifyWrapper.erase(uID);
     }
-        break;
+    break;
     }
 }
 
 Application::Application()
-: _instance(nullptr)
-, _accelTable(nullptr)
-{
-    _instance    = GetModuleHandle(nullptr);
+    : _instance(nullptr)
+    , _accelTable(nullptr) {
+    _instance = GetModuleHandle(nullptr);
     _animationInterval.QuadPart = 0;
-    CC_ASSERT(! sm_pSharedApplication);
+    CC_ASSERT(!sm_pSharedApplication);
     sm_pSharedApplication = this;
 }
 
-Application::~Application()
-{
+Application::~Application() {
     CC_ASSERT(this == sm_pSharedApplication);
     sm_pSharedApplication = nullptr;
 }
@@ -195,7 +194,7 @@ void Application::Dialog(
 
 void Application::DialogMessageFilter() noexcept {
     static MSG msg = {};
-    if (PeekMessage(&msg, nullptr, 0, 0, PM_NOREMOVE) && VecDlgWrapper.size() > 0){
+    if (PeekMessage(&msg, nullptr, 0, 0, PM_NOREMOVE) && VecDlgWrapper.size() > 0) {
         for (auto& wrapper : VecDlgWrapper) {
             if (IsDialogMessage(wrapper.dlgWnd, &msg)) {
                 return;
@@ -229,15 +228,15 @@ void Application::Notify(
     nd.dwInfoFlags = NIIF_USER | NIIF_LARGE_ICON;
     nd.uVersion = NOTIFYICON_VERSION_4;
 
-    Shell_NotifyIcon(NIM_ADD,&nd);
+    Shell_NotifyIcon(NIM_ADD, &nd);
     Shell_NotifyIcon(NIM_SETVERSION, &nd);
 
     MapNotifyWrapper.emplace(NotifyID, NotifyWrapper{ clickCallback,closeCallback });
     NotifyID++;
 }
 
-int Application::run()
-{
+int Application::run() {
+
     PVRFrameEnableControlWindow(false);
 
     ///////////////////////////////////////////////////////////////////////////
@@ -246,8 +245,7 @@ int Application::run()
     constexpr UINT TARGET_RESOLUTION = 1; // 1 millisecond target resolution
     TIMECAPS tc = {};
     UINT wTimerRes = 0;
-    if (TIMERR_NOERROR == timeGetDevCaps(&tc, sizeof(TIMECAPS)))
-    {
+    if (TIMERR_NOERROR == timeGetDevCaps(&tc, sizeof(TIMECAPS))) {
         wTimerRes = std::min(std::max(tc.wPeriodMin, TARGET_RESOLUTION), tc.wPeriodMax);
         timeBeginPeriod(wTimerRes);
     }
@@ -261,8 +259,7 @@ int Application::run()
     initGLContextAttrs();
 
     // Initialize instance and cocos2d.
-    if (!applicationDidFinishLaunching())
-    {
+    if (!applicationDidFinishLaunching()) {
         return 1;
     }
 
@@ -280,19 +277,17 @@ int Application::run()
 
     g_oldWindowProc = (WNDPROC)SetWindowLongPtr(glview->getWin32Window(), GWLP_WNDPROC, (LONG_PTR)WindowProc);
 
-    while(!glview->windowShouldClose())
-    {
+    while (!glview->windowShouldClose()) {
         QueryPerformanceCounter(&nNow);
         interval = nNow.QuadPart - nLast.QuadPart;
-        if (interval >= _animationInterval.QuadPart)
-        {
+        if (interval >= _animationInterval.QuadPart) {
             nLast.QuadPart = nNow.QuadPart;
+            CefDoMessageLoopWork();
             DialogMessageFilter();
             director->mainLoop();
             glview->pollEvents();
         }
-        else
-        {
+        else {
             // The precision of timer on Windows is set to highest (1ms) by 'timeBeginPeriod' from above code,
             // but it's still not precise enough. For example, if the precision of timer is 1ms,
             // Sleep(3) may make a sleep of 2ms or 4ms. Therefore, we subtract 1ms here to make Sleep time shorter.
@@ -305,8 +300,7 @@ int Application::run()
     }
 
     // Director should still do a cleanup if the window was closed manually.
-    if (glview->isOpenGLReady())
-    {
+    if (glview->isOpenGLReady()) {
         director->end();
         director->mainLoop();
         director = nullptr;
@@ -316,15 +310,13 @@ int Application::run()
     ///////////////////////////////////////////////////////////////////////////
     /////////////// restoring timer resolution
     ///////////////////////////////////////////////////////////////////////////
-    if (wTimerRes != 0)
-    {
+    if (wTimerRes != 0) {
         timeEndPeriod(wTimerRes);
     }
     return 0;
 }
 
-void Application::setAnimationInterval(float interval)
-{
+void Application::setAnimationInterval(float interval) {
     LARGE_INTEGER freq;
     QueryPerformanceFrequency(&freq);
     _animationInterval.QuadPart = (LONGLONG)(interval * freq.QuadPart);
@@ -333,88 +325,84 @@ void Application::setAnimationInterval(float interval)
 //////////////////////////////////////////////////////////////////////////
 // static member function
 //////////////////////////////////////////////////////////////////////////
-Application* Application::getInstance()
-{
+Application* Application::getInstance() {
     CC_ASSERT(sm_pSharedApplication);
     return sm_pSharedApplication;
 }
 
-LanguageType Application::getCurrentLanguage()
-{
+LanguageType Application::getCurrentLanguage() {
     LanguageType ret = LanguageType::ENGLISH;
 
     LCID localeID = GetUserDefaultLCID();
     unsigned short primaryLanguageID = localeID & 0xFF;
 
-    switch (primaryLanguageID)
-    {
-        case LANG_CHINESE:
-            ret = LanguageType::CHINESE;
-            break;
-        case LANG_ENGLISH:
-            ret = LanguageType::ENGLISH;
-            break;
-        case LANG_FRENCH:
-            ret = LanguageType::FRENCH;
-            break;
-        case LANG_ITALIAN:
-            ret = LanguageType::ITALIAN;
-            break;
-        case LANG_GERMAN:
-            ret = LanguageType::GERMAN;
-            break;
-        case LANG_SPANISH:
-            ret = LanguageType::SPANISH;
-            break;
-        case LANG_DUTCH:
-            ret = LanguageType::DUTCH;
-            break;
-        case LANG_RUSSIAN:
-            ret = LanguageType::RUSSIAN;
-            break;
-        case LANG_KOREAN:
-            ret = LanguageType::KOREAN;
-            break;
-        case LANG_JAPANESE:
-            ret = LanguageType::JAPANESE;
-            break;
-        case LANG_HUNGARIAN:
-            ret = LanguageType::HUNGARIAN;
-            break;
-        case LANG_PORTUGUESE:
-            ret = LanguageType::PORTUGUESE;
-            break;
-        case LANG_ARABIC:
-            ret = LanguageType::ARABIC;
-            break;
-        case LANG_NORWEGIAN:
-            ret = LanguageType::NORWEGIAN;
-            break;
-        case LANG_POLISH:
-            ret = LanguageType::POLISH;
-            break;
-        case LANG_TURKISH:
-            ret = LanguageType::TURKISH;
-            break;
-        case LANG_UKRAINIAN:
-            ret = LanguageType::UKRAINIAN;
-            break;
-        case LANG_ROMANIAN:
-            ret = LanguageType::ROMANIAN;
-            break;
-        case LANG_BULGARIAN:
-            ret = LanguageType::BULGARIAN;
-            break;
-        case LANG_BELARUSIAN:
-            ret = LanguageType::BELARUSIAN;
-            break;
+    switch (primaryLanguageID) {
+    case LANG_CHINESE:
+        ret = LanguageType::CHINESE;
+        break;
+    case LANG_ENGLISH:
+        ret = LanguageType::ENGLISH;
+        break;
+    case LANG_FRENCH:
+        ret = LanguageType::FRENCH;
+        break;
+    case LANG_ITALIAN:
+        ret = LanguageType::ITALIAN;
+        break;
+    case LANG_GERMAN:
+        ret = LanguageType::GERMAN;
+        break;
+    case LANG_SPANISH:
+        ret = LanguageType::SPANISH;
+        break;
+    case LANG_DUTCH:
+        ret = LanguageType::DUTCH;
+        break;
+    case LANG_RUSSIAN:
+        ret = LanguageType::RUSSIAN;
+        break;
+    case LANG_KOREAN:
+        ret = LanguageType::KOREAN;
+        break;
+    case LANG_JAPANESE:
+        ret = LanguageType::JAPANESE;
+        break;
+    case LANG_HUNGARIAN:
+        ret = LanguageType::HUNGARIAN;
+        break;
+    case LANG_PORTUGUESE:
+        ret = LanguageType::PORTUGUESE;
+        break;
+    case LANG_ARABIC:
+        ret = LanguageType::ARABIC;
+        break;
+    case LANG_NORWEGIAN:
+        ret = LanguageType::NORWEGIAN;
+        break;
+    case LANG_POLISH:
+        ret = LanguageType::POLISH;
+        break;
+    case LANG_TURKISH:
+        ret = LanguageType::TURKISH;
+        break;
+    case LANG_UKRAINIAN:
+        ret = LanguageType::UKRAINIAN;
+        break;
+    case LANG_ROMANIAN:
+        ret = LanguageType::ROMANIAN;
+        break;
+    case LANG_BULGARIAN:
+        ret = LanguageType::BULGARIAN;
+        break;
+    case LANG_BELARUSIAN:
+        ret = LanguageType::BELARUSIAN;
+        break;
     }
 
     return ret;
 }
 
-const char * Application::getCurrentLanguageCode()
-{
+const char* Application::getCurrentLanguageCode() {
     LANGID lid = GetUserDefaultUILanguage();
     const LCID locale_id = MAKELCID(lid, SORT_DEFAULT);
     static char code[3] = { 0 };
@@ -423,13 +411,11 @@ const char * Application::getCurrentLanguageCode()
     return code;
 }
 
-Application::Platform Application::getTargetPlatform()
-{
+Application::Platform Application::getTargetPlatform() {
     return Platform::OS_WINDOWS;
 }
 
-std::string Application::getVersion()
-{
+std::string Application::getVersion() {
     char verString[256] = { 0 };
     TCHAR szVersionFile[MAX_PATH];
     GetModuleFileName(NULL, szVersionFile, MAX_PATH);
@@ -438,28 +424,23 @@ std::string Application::getVersion()
     LPBYTE lpBuffer = NULL;
     DWORD  verSize = GetFileVersionInfoSize(szVersionFile, &verHandle);
 
-    if (verSize != NULL)
-    {
+    if (verSize != NULL) {
         LPSTR verData = new char[verSize];
 
-        if (GetFileVersionInfo(szVersionFile, verHandle, verSize, verData))
-        {
-            if (VerQueryValue(verData, L"\\", (VOID FAR* FAR*)&lpBuffer, &size))
-            {
-                if (size)
-                {
-                    VS_FIXEDFILEINFO *verInfo = (VS_FIXEDFILEINFO *)lpBuffer;
-                    if (verInfo->dwSignature == 0xfeef04bd)
-                    {
+        if (GetFileVersionInfo(szVersionFile, verHandle, verSize, verData)) {
+            if (VerQueryValue(verData, L"\\", (VOID FAR * FAR*) & lpBuffer, &size)) {
+                if (size) {
+                    VS_FIXEDFILEINFO* verInfo = (VS_FIXEDFILEINFO*)lpBuffer;
+                    if (verInfo->dwSignature == 0xfeef04bd) {
 
                         // Doesn't matter if you are on 32 bit or 64 bit,
                         // DWORD is always 32 bits, so first two revision numbers
                         // come from dwFileVersionMS, last two come from dwFileVersionLS
                         sprintf(verString, "%d.%d.%d.%d", (verInfo->dwFileVersionMS >> 16) & 0xffff,
-                                (verInfo->dwFileVersionMS >> 0) & 0xffff,
-                                (verInfo->dwFileVersionLS >> 16) & 0xffff,
-                                (verInfo->dwFileVersionLS >> 0) & 0xffff
-                                );
+                            (verInfo->dwFileVersionMS >> 0) & 0xffff,
+                            (verInfo->dwFileVersionLS >> 16) & 0xffff,
+                            (verInfo->dwFileVersionLS >> 0) & 0xffff
+                        );
                     }
                 }
             }
@@ -498,21 +479,18 @@ int64_t Application::GetCompileVersion() {
     return compileVer;
 }
 
-bool Application::openURL(const std::string &url)
-{
-    WCHAR *temp = new WCHAR[url.size() + 1];
+bool Application::openURL(const std::string& url) {
+    WCHAR* temp = new WCHAR[url.size() + 1];
     int wchars_num = MultiByteToWideChar(CP_UTF8, 0, url.c_str(), url.size() + 1, temp, url.size() + 1);
     HINSTANCE r = ShellExecuteW(NULL, L"open", temp, NULL, NULL, SW_SHOWNORMAL);
     delete[] temp;
-    return (size_t)r>32;
+    return (size_t)r > 32;
 }
 
-void Application::setResourceRootPath(const std::string& rootResDir)
-{
+void Application::setResourceRootPath(const std::string& rootResDir) {
     _resourceRootPath = rootResDir;
     std::replace(_resourceRootPath.begin(), _resourceRootPath.end(), '\\', '/');
-    if (_resourceRootPath[_resourceRootPath.length() - 1] != '/')
-    {
+    if (_resourceRootPath[_resourceRootPath.length() - 1] != '/') {
         _resourceRootPath += '/';
     }
     FileUtils* pFileUtils = FileUtils::getInstance();
@@ -521,13 +499,11 @@ void Application::setResourceRootPath(const std::string& rootResDir)
     pFileUtils->setSearchPaths(searchPaths);
 }
 
-const std::string& Application::getResourceRootPath(void)
-{
+const std::string& Application::getResourceRootPath(void) {
     return _resourceRootPath;
 }
 
-void Application::setStartupScriptFilename(const std::string& startupScriptFile)
-{
+void Application::setStartupScriptFilename(const std::string& startupScriptFile) {
     _startupScriptFilename = startupScriptFile;
     std::replace(_startupScriptFilename.begin(), _startupScriptFilename.end(), '\\', '/');
 }
@@ -537,12 +513,11 @@ NS_CC_END
 //////////////////////////////////////////////////////////////////////////
 // Local function
 //////////////////////////////////////////////////////////////////////////
-static void PVRFrameEnableControlWindow(bool bEnable)
-{
+static void PVRFrameEnableControlWindow(bool bEnable) {
     HKEY hKey = 0;
 
     // Open PVRFrame control key, if not exist create it.
-    if(ERROR_SUCCESS != RegCreateKeyExW(HKEY_CURRENT_USER,
+    if (ERROR_SUCCESS != RegCreateKeyExW(HKEY_CURRENT_USER,
         L"Software\\Imagination Technologies\\PVRVFRame\\STARTUP\\",
         0,
         0,
@@ -550,22 +525,21 @@ static void PVRFrameEnableControlWindow(bool bEnable)
         KEY_ALL_ACCESS,
         0,
         &hKey,
-        nullptr))
-    {
+        nullptr)) {
         return;
     }
 
     const WCHAR* wszValue = L"hide_gui";
     const WCHAR* wszNewData = (bEnable) ? L"NO" : L"YES";
-    WCHAR wszOldData[256] = {0};
+    WCHAR wszOldData[256] = { 0 };
     DWORD   dwSize = sizeof(wszOldData);
     LSTATUS status = RegQueryValueExW(hKey, wszValue, 0, nullptr, (LPBYTE)wszOldData, &dwSize);
     if (ERROR_FILE_NOT_FOUND == status              // the key not exist
         || (ERROR_SUCCESS == status                 // or the hide_gui value is exist
-        && 0 != wcscmp(wszNewData, wszOldData)))    // but new data and old data not equal
+            && 0 != wcscmp(wszNewData, wszOldData)))    // but new data and old data not equal
     {
         dwSize = sizeof(WCHAR) * (wcslen(wszNewData) + 1);
-        RegSetValueEx(hKey, wszValue, 0, REG_SZ, (const BYTE *)wszNewData, dwSize);
+        RegSetValueEx(hKey, wszValue, 0, REG_SZ, (const BYTE*)wszNewData, dwSize);
     }
 
     RegCloseKey(hKey);
--- cocos/ui/UIWebView/UIWebViewImpl-windows.h
+++ cocos/ui/UIWebView/UIWebViewImpl-windows.h
@@ -1,61 +1,161 @@
-#ifndef __COCOS2D__UI__WEBVIEWIMPL_WIN32_H_
-#define __COCOS2D__UI__WEBVIEWIMPL_WIN32_H_
+/****************************************************************************
+ Copyright (c) 2014-2016 Chukong Technologies Inc.
+ Copyright (c) 2017-2018 Xiamen Yaji Software Co., Ltd.
 
-#include "platform/CCPlatformConfig.h"
+ http://www.cocos2d-x.org
 
-#if CC_TARGET_PLATFORM == CC_PLATFORM_WIN32
+ Permission is hereby granted, free of charge, to any person obtaining a copy
+ of this software and associated documentation files (the "Software"), to deal
+ in the Software without restriction, including without limitation the rights
+ to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ copies of the Software, and to permit persons to whom the Software is
+ furnished to do so, subject to the following conditions:
+
+ The above copyright notice and this permission notice shall be included in
+ all copies or substantial portions of the Software.
+
+ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ THE SOFTWARE.
+ ****************************************************************************/
+
+#ifndef __COCOS2D_UI_WEBVIEWIMPL_WIN32_H_
+#define __COCOS2D_UI_WEBVIEWIMPL_WIN32_H_
+ /// @cond DO_NOT_SHOW
 
 #include <string>
-#include "platform/CCStdC.h"
+#include "include/base/cef_lock.h"
+#include "include/cef_client.h"
+#include "include/cef_parser.h"
 
 namespace cocos2d {
+
     class Data;
     class Renderer;
     class Mat4;
 
     namespace ui {
+
         class WebView;
-    }
-}
 
-class Win32WebControl;
+        class WebViewImpl :
+            public CefClient,
+            public CefLifeSpanHandler,
+            public CefLoadHandler,
+            public CefContextMenuHandler,
+            public CefRequestHandler {
+        public:
+            WebViewImpl(WebView* webView);
+            virtual ~WebViewImpl();
+            void setJavascriptInterfaceScheme(const std::string& scheme);
+            void loadData(const cocos2d::Data& data,
+                const std::string& MIMEType,
+                const std::string& encoding,
+                const std::string& baseURL);
+            void loadHTMLString(const std::string& string, const std::string& baseURL);
+            void loadURL(const std::string& url);
+            void loadURL(const std::string& url, bool cleanCachedData);
+            void loadFile(const std::string& fileName);
+            void stopLoading();
+            void reload();
+            bool canGoBack();
+            bool canGoForward();
+            void goBack();
+            void goForward();
+            void evaluateJS(const std::string& js);
+            void setScalesPageToFit(const bool scalesPageToFit);
+            virtual void draw(cocos2d::Renderer* renderer, cocos2d::Mat4 const& transform, uint32_t flags);
+            virtual void setVisible(bool visible);
+            void setBounces(bool bounces);
+            virtual void setOpacityWebView(float opacity);
+            virtual float getOpacityWebView() const;
+            virtual void setBackgroundTransparent();
 
-namespace cocos2d {
-namespace ui {
+        public:
 
-    class WebViewImpl
-    {
-    public:
-        WebViewImpl(cocos2d::ui::WebView *webView);
-        virtual ~WebViewImpl();
+            virtual CefRefPtr<CefLifeSpanHandler> GetLifeSpanHandler() OVERRIDE { return this; }
+            virtual CefRefPtr<CefLoadHandler> GetLoadHandler() OVERRIDE { return this; }
+            virtual CefRefPtr<CefContextMenuHandler> GetContextMenuHandler() OVERRIDE { return this; }
+            virtual CefRefPtr<CefRequestHandler> GetRequestHandler() OVERRIDE { return this; }
 
-        void setJavascriptInterfaceScheme(const std::string &scheme);
-        void loadData(const cocos2d::Data &data, const std::string &MIMEType, const std::string &encoding, const std::string &baseURL);
-        void loadHTMLString(const std::string &string, const std::string &baseURL);
-        void loadURL(const std::string &url);
-        void loadFile(const std::string &fileName);
-        void stopLoading();
-        void reload();
-        bool canGoBack();
-        bool canGoForward();
-        void goBack();
-        void goForward();
-        void evaluateJS(const std::string &js);
-        void setScalesPageToFit(const bool scalesPageToFit);
+            // CefLifeSpanHandler methods:
+            virtual void OnAfterCreated(CefRefPtr<CefBrowser> browser) OVERRIDE;
+            virtual bool DoClose(CefRefPtr<CefBrowser> browser) OVERRIDE;
+            virtual void OnBeforeClose(CefRefPtr<CefBrowser> browser) OVERRIDE;
+            virtual bool OnBeforePopup(
+                CefRefPtr<CefBrowser> browser,
+                CefRefPtr<CefFrame> frame,
+                const CefString& target_url,
+                const CefString& target_frame_name,
+                CefLifeSpanHandler::WindowOpenDisposition target_disposition,
+                bool user_gesture,
+                const CefPopupFeatures& popupFeatures,
+                CefWindowInfo& windowInfo,
+                CefRefPtr<CefClient>& client,
+                CefBrowserSettings& settings,
+                CefRefPtr<CefDictionaryValue>& extra_info,
+                bool* no_javascript_access
+            ) OVERRIDE;
 
-        virtual void draw(cocos2d::Renderer *renderer, cocos2d::Mat4 const &transform, uint32_t flags);
-        virtual void setVisible(bool visible);
+            // CefLoadHandler methods:
+            virtual void OnLoadEnd(CefRefPtr<CefBrowser> browser,
+                CefRefPtr<CefFrame> frame,
+                int httpStatusCode) OVERRIDE;
+            virtual void OnLoadError(CefRefPtr<CefBrowser> browser,
+                CefRefPtr<CefFrame> frame,
+                ErrorCode errorCode,
+                const CefString& errorText,
+                const CefString& failedUrl) OVERRIDE;
 
-		void setBounces(bool bounces);
+            // CefContextMenuHandler methods:
+            virtual void OnBeforeContextMenu(CefRefPtr<CefBrowser> browser,
+                CefRefPtr<CefFrame> frame,
+                CefRefPtr<CefContextMenuParams> params,
+                CefRefPtr<CefMenuModel> model) OVERRIDE;
 
-    private:
-        bool _createSucceeded;
-        Win32WebControl *_systemWebControl;
-        WebView *_webView;
-    };
-} // namespace ui
-} //cocos2d
+            // CefRequestHandler methods:
+            bool OnBeforeBrowse(CefRefPtr<CefBrowser> browser,
+                CefRefPtr<CefFrame> frame,
+                CefRefPtr<CefRequest> request,
+                bool user_gesture,
+                bool is_redirect) OVERRIDE;
 
-#endif // CC_TARGET_PLATFORM == CC_PLATFORM_WIN32
+        private:
+            // Create a new browser and native window.
+            void CreateCefBrowser();
+            void ResizeMoveCefBrowser();
+            void DestoryCefBrowser();
+            bool BrowserValid() const {
+                return mBrowser && !mIsClosed && !mIsClosing;
+            };
 
-#endif // __COCOS2D__UI__WEBVIEWIMPL_WIN32_H_
+            static std::string GetDataURI(const std::string& data, const std::string& mime_type) {
+                return "data:" + mime_type +
+                    ";base64," + CefURIEncode(CefBase64Encode(data.data(), data.size()), false).ToString();
+            }
+
+            WebView* _webView;
+            HWND mHWND = nullptr;
+            CefRefPtr<CefBrowser> mBrowser = nullptr;
+            bool mIsClosing = false;
+            bool mIsClosed = false;
+            bool mScalesPageToFit = false;
+            bool mVisible = true;
+            std::string mJsScheme = std::string();
+
+            // Include filename or url or string.
+            std::string mUrl = std::string();
+
+            // Include the default reference counting implementation.
+            IMPLEMENT_REFCOUNTING(WebViewImpl);
+            DISALLOW_COPY_AND_ASSIGN(WebViewImpl);
+        };
+    } // namespace ui
+}//namespace cocos2d
+
+/// @endcond
+#endif /* __COCOS2D_UI_WEBVIEWIMPL_WIN32_H_ */
