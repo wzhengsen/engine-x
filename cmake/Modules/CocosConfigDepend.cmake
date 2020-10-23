872a8a60fb0168c975c2741a1dac355287870782
wzhengsen <wzhengsen@gmail.com>
2020-10-24 01:05:22 +0800

    [new]Device feature for macos and ios locate.

--- cmake/Modules/CocosConfigDepend.cmake
+++ cmake/Modules/CocosConfigDepend.cmake
@@ -61,6 +61,7 @@ macro(cocos2dx_depend)
             find_library(ICONV_LIBRARY iconv)
             find_library(AUDIOUNIT_LIBRARY AudioUnit)
             find_library(COREAUDIO_LIBRARY CoreAudio)
+            find_library(COREWLAN_LIBRARY CoreWLAN)
             list(APPEND PLATFORM_SPECIFIC_LIBS
                  ${COCOA_LIBRARY}
                  ${OPENGL_LIBRARY}
@@ -71,6 +72,7 @@ macro(cocos2dx_depend)
                  ${ICONV_LIBRARY}
                  ${AUDIOUNIT_LIBRARY}
                  ${COREAUDIO_LIBRARY}
+                 ${COREWLAN_LIBRARY}
                  )
         elseif(IOS)
             # Locate system libraries on iOS
