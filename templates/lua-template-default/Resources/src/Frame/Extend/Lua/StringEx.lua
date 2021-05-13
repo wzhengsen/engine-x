a06034a6e9eb3e089f7b6a2858e41375dce03407
wzhengsen <wzhengsen@gmail.com>
2021-05-13 17:56:47 +0800

    [new]string.Join

--- templates/lua-template-default/Resources/src/Frame/Extend/Lua/StringEx.lua
+++ templates/lua-template-default/Resources/src/Frame/Extend/Lua/StringEx.lua
@@ -9,48 +9,54 @@ Update: 新增了加解密（Encrypt,Decrypt）、
         哈希值（Hash）
         的算法
 ---------------------------------------------]]
-local crypto = require("crypto")
+local crypto = require("crypto");
 
 --	Func:	截取
 --	Param:	string,string(截取符集合)
 --	Return:	table
 ---------------------------------------------
-function string.Split(s,sep,cFunc)
-    local fields = {}
-    s:gsub("([^"..sep.."]*)",
-    function(c)
-        if cFunc then
-            c = cFunc(c)
+function string:Split(sep, cFunc)
+    local fields = {};
+    self:gsub(
+        "([^" .. sep .. "]*)",
+        function(c)
+            if cFunc then
+                c = cFunc(c);
+            end
+            fields[#fields + 1] = c;
         end
-        fields[#fields + 1] = c
-    end)
-    return fields
+    )
+    return fields;
 end
 
 --	Func:	除前后空白符
 --	Param:	string
 --	Return:	string
 ---------------------------------------------
-function string.Trim(s)
-    return s:match("%S.*%S") or ""
+function string:Trim()
+    return self:match("%S.*%S") or "";
 end
 
-function string.EncodeUrl(input)
+local function EncodeUrl(input)
     -- convert line endings
-    return input:gsub("\n","\r\n"):
-    -- escape all characters but alphanumeric, -_.!~*'()
-    gsub("([^%w%-%_%.%!%~%*%'%(%) ])",function(char) return "%" .. ("%02X"):format(char:byte()) end):
+    return input:gsub("\n", "\r\n"):gsub(
+        -- escape all characters but alphanumeric, -_.!~*'()
+        "([^%w%-%_%.%!%~%*%'%(%) ])",
+        function(char)
+            return "%" .. ("%02X"):format(char:byte());
+        end
+    ):gsub(" ", "+");
     -- convert spaces to "+" symbols
-    gsub(" ","+")
 end
-string.EncodeURL = string.EncodeUrl;
 
-function string.DecodeUrl(input)
-    return input:gsub("+"," "):
-    gsub("%%(%x%x)",function(h) return string.char(tonumber(h,16)) end):
-    gsub("\r\n", "\n")
+local function DecodeUrl(input)
+    return input:gsub("+", " "):gsub(
+        "%%(%x%x)",
+        function(h)
+            return string.char(tonumber(h, 16));
+        end
+    ):gsub("\r\n", "\n");
 end
-string.DecodeURL = string.DecodeUrl;
 
 --[[
     Func:   编码解码
@@ -62,24 +68,24 @@ string.DecodeURL = string.DecodeUrl;
             }编解码类型
     Return: string
 ]]
-function string.Encode(str,eType)
-    eType = eType:upper()
-    local lib = eType == "URL" and string or crypto
-    local encode = lib["Encode"..eType]
+function string:Encode(eType)
+    eType = eType:upper();
+    local encode = eType == "URL" and EncodeUrl or crypto["Encode" .. eType];
     if encode then
         -- 强制返回一个值。
-        return (encode(str));
+        return (encode(self));
     end
+    return nil;
 end
 
-function string.Decode(str,eType)
-    eType = eType:upper()
-    local lib = eType == "URL" and string or crypto
-    local decode = lib["Decode"..eType]
+function string:Decode(eType)
+    eType = eType:upper();
+    local decode = eType == "URL" and DecodeUrl or crypto["Decode" .. eType];
     if decode then
         -- 强制返回一个值。
-        return (decode(str));
+        return (decode(self));
     end
+    return nil;
 end
 
 
@@ -97,13 +103,13 @@ end
             boolean{true}   取哈希后是否进行16进制编码
     Return: string
 ]]
-function string.Hash(str,eType,enc)
+function string:Hash(eType, enc)
     local hash = crypto[eType:upper()];
     if hash then
         if enc == nil or enc then
-            return hash(str):Encode("hex");
+            return hash(self):Encode("hex");
         end
-        return hash(str);
+        return hash(self);
     end
 end
 
@@ -123,5 +129,12 @@ string.Decrypt = crypto.Decrypt;
     Desc:       获取一个尽量不重复的字符串。
 ]]
 function string.Unique()
-    return (tostring({}) .. os.time() .. os.clock() .. math.random(1,1000000)):Hash("sha1");
+    return (tostring({}) .. os.time() .. os.clock() .. math.random(1, 1000000)):Hash("sha1");
+end
+
+--[[
+    Param:  table
+]]
+function string:Join(t)
+    return table.concat(t, self);
 end
