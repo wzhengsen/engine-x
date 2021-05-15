ebf3dbdd816d4acbc4c91492cf25d2f79e59970e
wzhengsen <wzhengsen@gmail.com>
2021-05-15 18:09:25 +0800

    [chg]LuaPanda and main.

--- templates/lua-template-default/Resources/src/main.lua
+++ templates/lua-template-default/Resources/src/main.lua
@@ -1,10 +1,17 @@
+xpcall(
+function()
+    require("src.LuaPanda").start();
+end,
+function(msg)
+    print(msg);
+end);
+
 local function main()
     package.path = package.path .. ";src/?.lua";
-    require("LuaPanda").start();
     require("Frame.Init");
     syx.Application():Start();
 end
-
-xpcall(main,function (msg)
+xpcall(main,
+function(msg)
     print(msg);
 end);
